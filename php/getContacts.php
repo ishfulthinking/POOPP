27cc922f31d6d126b34e09701bbbeeaf8f7fa4e5
Anuj Yamdagni <Anuj@AnujMacbook.local>
2018-09-10 21:31:14 -0400

    getContacts to get all contacts in db

---
+++ php/getContacts.php
@@ -0,0 +1,39 @@
+<?php
+/*if(!isset($_SESSION['login_user'])) {
+  die("error not logged in");
+}
+*/
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "ContactBook";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("error");
+}
+/*
+$user = $_SESSION['user'];
+$user = stripslashes($user);
+*/
+// get the q parameter from URL
+if(isset($_REQUEST["q"])) {
+$q = stripslashes($_REQUEST["q"]);
+} else {
+  $q = "";
+}
+
+$sql = "SELECT Name, Phone, Email FROM Contacts WHERE Name LIKE '$q%'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row["Name"] . ", " . $row["Phone"] .  ", " . $row["Email"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+?>
